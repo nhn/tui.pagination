729afec45e9255765d67de8d4f82b0c12fc02224
jein.yi <ahin1127>
2014-11-17 22:25:25 +0900

    karma

--- karma.conf.js
+++ karma.conf.js
@@ -75,7 +75,7 @@ module.exports = function(config) {
 
 
         // enable / disable watching file and executing tests whenever any file changes
-        autoWatch: false,
+        autoWatch: true,
 
 
         // start these browsers
@@ -137,6 +137,6 @@ module.exports = function(config) {
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: false
+        singleRun: true
     });
 };
