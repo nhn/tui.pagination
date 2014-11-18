037f34b6831296085b7f8b6b6b84a30d9a460521
jein.yi <ahin1127>
2014-11-18 16:28:19 +0900

    not used codes are deleted

--- src/js/common.js
+++ src/js/common.js
@@ -502,28 +502,9 @@
         lastId = 0;
     }
 
-    /**
-     * 객체를 전달받아 객체의 키목록을 배열로만들어 리턴해준다.
-     * @param obj
-     * @returns {Array}
-     */
-    var keys = function(obj) {
-        var keys = [],
-            key;
-
-        for (key in obj) {
-            if (obj.hasOwnProperty(key)) {
-                keys.push(key);
-            }
-        }
-
-        return keys;
-    };
-
     ne.extend = extend;
     ne.stamp = stamp;
     ne._resetLastId = resetLastId;
-    ne.keys = Object.keys || keys;
 
 })(window.ne);
 /**
@@ -547,38 +528,8 @@
         return obj !== null && obj !== undefined;
     }
 
-    /**
-     * 인자가 null, undefined, false가 아닌지 확인하는 메서드
-     * @param {*} obj
-     * @return {boolean}
-     */
-    function isTruthy(obj) {
-        return isDefined(obj) && obj !== false;
-    }
-
-    /**
-     * 인자가 null, undefined, false인지 확인하는 메서드
-     * @param {*} obj
-     * @return {boolean}
-     */
-    function isFalsy(obj) {
-        return !isTruthy(obj);
-    }
-
 
     var toString = Object.prototype.toString;
-
-    /**
-     * 인자가 arguments 객체인지 확인
-     * @param {*} obj
-     * @return {boolean}
-     */
-    function isArguments(obj) {
-        var result = isDefined(obj) &&
-            ((toString.call(obj) === '[object Arguments]') || 'callee' in obj);
-
-        return result;
-    }
 
     /**
      * 인자가 배열인지 확인
@@ -598,107 +549,9 @@
         return obj === Object(obj);
     }
 
-    /**
-     * 인자가 함수인지 확인하는 메서드
-     * @param {*} obj
-     * @return {boolean}
-     */
-    function isFunction(obj) {
-        return toString.call(obj) === '[object Function]';
-    }
-
-    /**
-     * 인자가 숫자인지 확인하는 메서드
-     * @param {*} obj
-     * @return {boolean}
-     */
-    function isNumber(obj) {
-        return toString.call(obj) === '[object Number]';
-    }
-
-    /**
-     * 인자가 문자열인지 확인하는 메서드
-     * @param obj
-     * @return {boolean}
-     */
-    function isString(obj) {
-        return toString.call(obj) === '[object String]';
-    }
-
-    /**
-     * 인자가 불리언 타입인지 확인하는 메서드
-     * @param {*} obj
-     * @return {boolean}
-     */
-    function isBoolean(obj) {
-        return toString.call(obj) === '[object Boolean]';
-    }
-
-    /**
-     * HTMLElement타입 검사
-     * @param {HTMLElement} html
-     * @return {Boolean} HTMLElement 인지 여부
-     */
-    function isHTMLElement(html)	{
-        if(typeof(HTMLElement) === 'object') {
-            return(html && (html instanceof HTMLElement));
-        }
-        return !!(html && html.nodeType);
-    }
-
-    /**
-     * null, undefined 여부와 순회 가능한 객체의 순회가능 갯수가 0인지 체크한다.
-     * @param {*} obj 평가할 대상
-     * @return {boolean}
-     */
-    function isEmpty(obj) {
-        var key,
-            hasKey = false;
-
-        if (!isDefined(obj)) {
-            return true;
-        }
-
-        if (isArray(obj) || isArguments(obj)) {
-            return obj.length === 0;
-        }
-
-        if (isObject(obj) && !isFunction(obj)) {
-            ne.forEachOwnProperties(obj, function() {
-                hasKey = true;
-                return false;
-            });
-
-            return !hasKey;
-        }
-
-        return true;
-
-    }
-
-    /**
-     * isEmpty 메서드와 반대로 동작한다.
-     * @param {*} obj 평가할 대상
-     * @return {boolean}
-     */
-    function isNotEmpty(obj) {
-        return !isEmpty(obj);
-    }
-
-
     ne.isDefined = isDefined;
-    ne.isTruthy = isTruthy;
-    ne.isFalsy = isFalsy;
-    ne.isArguments = isArguments;
     ne.isArray = Array.isArray || isArray;
     ne.isObject = isObject;
-    ne.isFunction = isFunction;
-    ne.isNumber = isNumber;
-    ne.isString = isString;
-    ne.isBoolean = isBoolean;
-    ne.isHTMLElement = isHTMLElement;
-    ne.isEmpty = isEmpty;
-    ne.isNotEmpty = isNotEmpty;
 
 })(window.ne);
 /**
@@ -709,6 +562,7 @@
 
 (function(ne) {
     'use strict';
+    /* istanbul ignore if */
     if (!ne) {
         ne = window.ne = {};
     }
@@ -763,68 +617,7 @@
             ne.forEachOwnProperties(obj, iteratee, context);
         }
     };
-
-    /**
-     * 파라메터로 전달된 객체나 어레이를 순회하며 콜백을 실행한 리턴값을 배열로 만들어 리턴한다.
-     * @param {*} obj 순회할 객체
-     * @param {Function} iteratee 데이터가 전달될 콜백함수
-     * @param {*} [context] 콜백함수의 컨텍스트
-     * @returns {Array}
-     * @example
-     * map([0,1,2,3], function(value) {
-     *     return value + 1;
-     * });
-     *
-     * => [1,2,3,4];
-     */
-    var map = function(obj, iteratee, context) {
-        var resultArray = [];
-
-        ne.forEach(obj, function() {
-            resultArray.push(iteratee.apply(context || null, arguments));
-        });
-
-        return resultArray;
-    };
-
-    /**
-     * 파라메터로 전달된 객체나 어레이를 순회하며 콜백을 실행한 리턴값을 다음 콜백의 첫번째 인자로 넘겨준다.
-     * @param {*} obj 순회할 객체
-     * @param {Function} iteratee 데이터가 전달될 콜백함수
-     * @param {*} [context] 콜백함수의 컨텍스트
-     * @returns {*}
-     * @example
-     * reduce([0,1,2,3], function(stored, value) {
-     *     return stored + value;
-     * });
-     *
-     * => 6;
-     */
-    var reduce = function(obj, iteratee, context) {
-        var keys,
-            index = 0,
-            length,
-            store;
-
-
-        if (!ne.isArray(obj)) {
-            keys = ne.keys(obj);
-        }
-
-        length = keys ? keys.length : obj.length;
-
-        store = obj[keys ? keys[index++] : index++];
-
-        for (; index < length; index++) {
-            store = iteratee.call(context || null, store, obj[keys ? keys[index] : index]);
-        }
-
-        return store;
-    };
-
     ne.forEachOwnProperties = forEachOwnProperties;
     ne.forEach = forEach;
-    ne.map = map;
-    ne.reduce = reduce;
 
 })(window.ne);
