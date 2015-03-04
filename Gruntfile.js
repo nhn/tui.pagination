31242e277c9d7ecb76a1b622b0d17142e13e1992
jein.yi <ahin1127>
2015-03-04 18:03:57 +0900

    grunt change

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         // You can set arbitrary key-value pairs.
         distFolder: 'dist',
-        name: 'Calendar',
+        name: 'calendar',
         // You can also set the value of a key as parsed JSON.
         // Allows us to reference properties we declared in package.json.
         pkg: grunt.file.readJSON('package.json'),
